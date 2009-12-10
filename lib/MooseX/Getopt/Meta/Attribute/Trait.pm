8b9e50c33133b5735d32f4b3d05eee82a919b382
Tomas Doran <bobtfish@bobtfish.net>
2009-12-10 23:21:36 +0000

    Bump version

--- lib/MooseX/Getopt/Meta/Attribute/Trait.pm
+++ lib/MooseX/Getopt/Meta/Attribute/Trait.pm
@@ -3,7 +3,7 @@ package MooseX::Getopt::Meta::Attribute::Trait;
 use Moose::Role;
 use Moose::Util::TypeConstraints;
 
-our $VERSION   = '0.25';
+our $VERSION   = '0.26';
 our $AUTHORITY = 'cpan:STEVAN';
 
 has 'cmd_flag' => (
